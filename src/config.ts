3435d5c3e1716d9ee814079a22d60a51328e30d6
happyjin <happyjin@tencent.com>
2023-02-06 17:11:17 +0800

    enter

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
   // 填入你的session token
-  OPENAI_API_KEY: '',
+  OPENAI_API_KEY: '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',
   // 设置获取消息的重试次数
   retryTimes: 3,
   // 在群组中设置唤醒微信机器人的关键词
